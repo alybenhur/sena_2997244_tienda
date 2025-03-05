67ae516f45a84df39e8d10b344476dd37efd955e
aly <alybenhur@gmail.com>
2025-03-05 12:42:36 -0500

    proyecto inicial tienda

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar extended>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon="mdi-dots-vertical">
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+  
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="data in bd" :key="data"
+            cols="3"
+          >
+           <tarjeta 
+           :datos="data"
+           :alto="altotarjeta"></tarjeta>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script>
+ import tarjeta from './components/TerjetaOnline.vue'
+export default {
+  components : {
+     tarjeta
+  },
+  data() {
+    return {
+      altotarjeta : 440,
+      bd : [
+        {
+          titulo : 'Shampoo Heard Shoulder',
+          descripcion : 'Shampoo para cabellos lisos, maltratados',
+          imagen : 'https://cdn1.totalcommerce.cloud/mercacentro/product-zoom/es/shampoo-h%26s-375-ml-limpieza-renovadora-1.webp',
+          valor : '30000'
+        },
+        {
+          titulo : 'Clorox Regular 1800 ml',
+          descripcion : 'Limpiador multiusos para el hogar y protege tus ojos',
+          imagen : 'https://cdn1.totalcommerce.cloud/mercacentro/product-thumb/es/blancox-corriente-megaoferta-3800-cm3-1.webp',
+          valor : '30000'
+        }
+      ]
+    }
+  },
+}
+</script>
---
+++ src/components/TerjetaOnline.vue
@@ -0,0 +1,75 @@
+   <template>
+            <v-card
+              :disabled="loading"
+              :loading="loading"
+              class="mx-auto my-12"
+              max-width="374"
+              :height="alto"
+            >
+            
+          
+              <v-img
+                height="250"
+                :src="datos.imagen"
+                contain
+              ></v-img>
+          
+              <v-card-item>
+
+                <v-card-title>{{datos.titulo}}</v-card-title>
+          
+                <v-card-subtitle>
+                  
+          
+                  <v-icon
+                    color="error"
+                    icon="mdi-fire-circle"
+                    size="small"
+                  ></v-icon>
+                </v-card-subtitle>
+
+              </v-card-item>
+          
+              <v-card-text>
+             
+              
+             
+          
+                <div>{{datos.descripcion}}</div>
+              </v-card-text>
+          
+              <v-divider class="mx-4 mb-1"></v-divider>
+          
+            
+          
+             
+          
+              <v-card-actions>
+                <v-btn
+                append-icon="mdi-cart-outline"
+                size="large"
+                block
+              >
+                <template v-slot:prepend>
+                  <v-icon color="success"></v-icon>
+                </template>
+          
+                <v-chip color="secondary" variant="flat">
+                    $COP {{datos.valor}}
+                  </v-chip>
+          
+                <template v-slot:append>
+                  <v-icon color="warning"
+                  
+                  ></v-icon>
+                </template>
+              </v-btn>
+              </v-card-actions>
+            </v-card>
+      
+</template>
+<script>
+export default {
+    props : ['datos','alto']
+}
+</script>
