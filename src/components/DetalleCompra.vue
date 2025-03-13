ba0523d711b9e9f03073d6655e7113396c2bea18
aly <alybenhur@gmail.com>
2025-03-13 17:02:26 -0500

    footer fectura

--- src/components/DetalleCompra.vue
+++ src/components/DetalleCompra.vue
@@ -14,6 +14,7 @@
           <v-data-table
            :headers="headers"
            :items="items"
+           :items-per-page="itemsPerPage"
           >
           <template v-slot:item="{ item }">
             <tr>
@@ -34,10 +35,51 @@
                  :cantidad ="item.cantidad"
                  :codigo="item.codigo"></ContadorCompra>
               </td>
+              <td>
+                <v-chip
+                 color="red"
+                 >
+                 {{ item.valor * item.cantidad }}
+                </v-chip>
+                
+              </td>
             </tr>
           </template>
+          <template v-slot:bottom>
+            <v-row justify="end">
+              <v-col cols="1">
+                 TOTAL
+              </v-col>
+              <v-col cols="2">
+                 hola
+              </v-col>
+             </v-row>
+            
+            <div class="custom-pagination d-flex align-center justify-space-between pa-4">
+              <!-- Controles de paginación originales -->
+              <v-pagination
+                v-model="page"
+                :length="pageCount"
+                :total-visible="7"
+                color="primary"
+                circle
+              ></v-pagination>
+              </div>
+              <div class="extra-controls d-flex align-center">
+                <v-select
+                  v-model="itemsPerPage"
+                  :items="itemsPerPageOptions"
+                  label="Elementos por página"
+                  dense
+                  outlined
+                  class="mr-4"
+                  style="max-width: 150px"
+                ></v-select>
 
-
+                </div>
+                
+             </template> 
+            
         </v-data-table>
 
           <template v-slot:actions>
@@ -70,9 +112,13 @@
             { title: 'Articulo', value: 'titulo' },
             { title: 'Imagen', value: 'imagen' },
             { title: 'Precio', value: 'valor' },
-            { title: 'Cantidad', value: 'cantidad' }
+            { title: 'Cantidad', value: 'cantidad' },
+            { title: 'Total' }
              ],
-             items : []
+             items : [],
+             page: 1,
+      itemsPerPage: 3,
+      itemsPerPageOptions: [3,5, 10, 15, -1],
            }
      },
    computed: {
